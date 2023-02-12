0b3a81faca768bd575b693ea56ead01bd6ab5691
Eyssette <cedric.eyssette@gmail.com>
2023-02-12 01:04:06 +0100

    Update getPhilosopherInfo.py
    Code plus lisible

--- script/getPhilosopherInfo.py
+++ script/getPhilosopherInfo.py
@@ -1,24 +1,25 @@
-import csv, urllib.request
+import csv
 import json
 import re
+import urllib.request
 
 def get_philosopher_info(philosopher_name):
-    query_url = "https://fr.wikipedia.org/w/api.php?action=query&format=json&prop=info|pageimages|extracts&exintro&generator=allpages&inprop=url&gaplimit=1&gapfrom=" + urllib.parse.quote(philosopher_name)
+    query_url = f"https://fr.wikipedia.org/w/api.php?action=query&format=json&prop=info|pageimages|extracts&exintro&generator=allpages&inprop=url&gaplimit=1&gapfrom={urllib.parse.quote(philosopher_name)}"
     try: 
-        wikipediaResponse = urllib.request.urlopen(query_url)
-        wikipediaResponseTxt = wikipediaResponse.read().decode('utf-8')
-        json_data = json.loads(wikipediaResponseTxt)
+        wikipedia_response = urllib.request.urlopen(query_url)
+        wikipedia_response_txt = wikipedia_response.read().decode('utf-8')
+        json_data = json.loads(wikipedia_response_txt)
         json_list = list(json_data['query'].values())[0]
         json_list2 = list(json_list.values())[0]
         if 'thumbnail' in json_list2:
-            img_link = json_list2['thumbnail']['source']
+            img_link = re.sub("/50px.*", '', json_list2['thumbnail']['source'].replace('/thumb', ''))
         else:
             img_link = ''
         url = json_list2['fullurl']
-        extract = json_list2['extract']
-        return img_link,url,extract
+        extract = re.sub('<!--.*?-->', '', json_list2['extract'].replace("\n", " "))
+        return img_link, url, extract
     except:
-        return ''
+        return '', '', ''
 
 url = 'https://raw.githubusercontent.com/eyssette/frise-philo/main/data/philosophers-1.csv'
 response = urllib.request.urlopen(url)
@@ -27,14 +28,6 @@ cr = csv.reader(lines)
 next(cr)
 
 for row in cr:
-    philosopher_name = row[0]
-    info_philosopher = get_philosopher_info(philosopher_name)
-    if info_philosopher[0]=='':
-        image_url =''
-    else :
-        image = re.sub("/50px.*",'',info_philosopher[0].replace('/thumb',''))
-        image_url='<img src="'+image+'" />'
-    url_wikipedia='<a href="'+info_philosopher[1]+'">Page wikipedia</a>'
-    extract_info=re.sub('<!--.*?-->','',info_philosopher[2].replace("\n", " "))
-    extract='<details>'+extract_info+'</details>'
-    print(f'{philosopher_name}\t{row[1]}\t{row[2]}\t{row[3]}\t{image_url}\t{url_wikipedia}\t{extract}')
+    philosopher_name, birth, death, philosophy, curriculum = row
+    img_link, url, extract = get_philosopher_info(philosopher_name)
+    print(f'{philosopher_name}\t{birth}\t{death}\t{philosophy}\t{img_link}\t{url}\t{extract}')

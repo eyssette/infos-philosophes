e2417731147c92ecdb6c5cdbc02bc12c9a40854a
eyssette <cedric.eyssette@gmail.com>
2023-02-17 09:15:47 +0100

    Ajout du nombre de philosophes dans la base

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -45,6 +45,7 @@
 	<p>Chargement des données. Merci de patienter.</p>
 {:then dataParsed}
 	<div class="search">
+		<p>{dataParsed.length} philosophes dans la base</p>
 		<Search bind:textToSearch />
 	</div>
 	<Table {dataParsed} bind:textToSearch />
